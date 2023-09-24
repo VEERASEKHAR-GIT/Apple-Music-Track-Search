4fd98a515b35cc1e729980cda987ce6beca0666d
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-23 22:26:41 -0500

    Update Apple-Music-Track-Search.py

--- Apple-Music-Track-Search.py
+++ Apple-Music-Track-Search.py
@@ -1,10 +1,15 @@
-import requests
+    artist_name = input("Enter the artist name: ")
+    collection_name = input("Enter the collection name (or leave blank for any collection): ")
+    track_name = input("Enter the track name (or leave blank for any track): ")
 
-def search_apple_music_tracks():
-    # Get the search term from the user
-    search_term = input("Enter a search term: ")
+    # Construct the search term based on the user inputs
+    search_term = artist_name
+    if collection_name:
+        search_term += f"+{collection_name}"
+    if track_name:
+        search_term += f"+{track_name}"
 
-    # Define the API URL with the search term
+    # Define the API URL with the constructed search term
     url = f"https://itunes.apple.com/search?entity=song&limit=10000&term={search_term}"
 
     # Send a GET request to the iTunes API
@@ -17,15 +22,13 @@ def search_apple_music_tracks():
 
         # Check if there are results in the response
         if "results" in data:
-            # Extract and print the track names
+            # Extract and print the track names, artist names, and collection names
             for result in data["results"]:
-                track_name = result.get("trackName", "N/A")
-                artist_name = result.get("artistName", "N/A")
-                print(f"Track: {track_name} - Artist: {artist_name}")
+                track_name_result = result.get("trackName", "{trackName}")
+                artist_name_result = result.get("artistName", "{artistName}")
+                collection_name_result = result.get("collectionName", "{collectionName}")
+                print(f"Track: {track_name_result} - Artist: {artist_name_result} - Collection: {collection_name_result}")
         else:
             print("No results found.")
     else:
         print(f"Error: {response.status_code} - {response.text}")
-
-if __name__ == "__main__":
-    search_apple_music_tracks()
